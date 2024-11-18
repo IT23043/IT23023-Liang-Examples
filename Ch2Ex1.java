e4d00113e81eae245c43d806e8a71a4296d8f37f
IT23043 <139215343+IT23043@users.noreply.github.com>
2024-11-18 01:21:24 -0500

    Add files via upload

---
+++ Ch2Ex1.java
@@ -0,0 +1,16 @@
+public class Ch2Ex1 {
+    public static void main(String[] args) {
+
+        double circleRadius; // Radius of the circle
+        double circleArea;   // Area of the circle
+
+        // Assign a value to the radius
+        circleRadius = 20.0;
+
+        // Compute the area
+        circleArea = circleRadius * circleRadius * 3.14159;
+
+        // Display the result
+        System.out.println("The area for the circle with radius " + circleRadius + " is " + circleArea);
+    }
+}
